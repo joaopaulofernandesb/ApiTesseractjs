43bceee5d56ce151662b4e54cb127c255997da93
Joao Brasil <joao_brasil@TSPD-NB-00475.tecnospeed.local>
2020-02-10 14:33:25 -0300

    ajuste

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import upload from './config/storage';
-import { extractText } from './lib/imageHandler';
+import express from "express";
+import bodyParser from "body-parser";
+import upload from "./config/storage";
+import { extractText } from "./lib/imageHandler";
+var cors = require("cors");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.post('/upload', upload.single('image'), async (req, res, next) => {
-  const file = req.file
+app.post("/upload", upload.single("image"), async (req, res, next) => {
+  const file = req.file;
   if (!file) {
-    const error = new Error('Envie o arquivo a ser tratado');
+    const error = new Error("Envie o arquivo a ser tratado");
     error.httpStatusCode = 400;
     return res.json(error);
   }
-  await extractText(file.path).then(text => {
-    const result = { data: text, ...file };
-    res.json(result);
-  }).catch(error => {
-    res.json(error);
-  });
-})
+  await extractText(file.path)
+    .then(text => {
+      const result = { data: text, ...file };
+      res.json(result);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!');
+app.listen(3000, function() {
+  console.log("Server listening on port 3000!");
 });
